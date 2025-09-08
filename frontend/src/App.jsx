94bacb90c807186fc58e48e7c79317e174922ecb
Stack <stack@Roopeshs-MacBook-Air.local>
2025-09-08 12:20:47 +0530

    Initializing FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>ConnectExpress</h1>
+      <p>Smart Railway Route Finder</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/context/README.md
@@ -0,0 +1,1 @@
+// Provides global state using React Context (AuthContext, SearchContext, etc.)
